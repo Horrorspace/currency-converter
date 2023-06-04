09edbe4e8efbe7e3bc47ffd5f6d6ca8b090a4fa7
Eugeniy Utkin <eugeniy.it@piar.ee>
2023-06-04 18:36:01 +0300

    renamed dto

--- src/currencies/dto/get-exchange-result.dto.ts
+++ src/currencies/dto/get-exchange-result.dto.ts
@@ -1,15 +1,28 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEnum, IsNumber, IsPositive, IsString, MinLength } from 'class-validator';
-import { Expose } from 'class-transformer';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEnum, IsNumber, IsOptional, IsPositive, IsString, Matches, MinLength } from 'class-validator';
+import { Expose, Transform } from 'class-transformer';
 import { IGetExchangeResult } from '../../shared/currencies/IGetExchangeResult';
 import { currencyCodeNames } from '../../shared/currencies/currency-code-names';
 import { currencyCodes } from '../../shared/currencies/currency-codes';
 import { currencyCode } from '../../shared/currencies/currency-code';
+import { baseCurrency } from '../../shared/currencies/base-currency';
+import { baseCurrencies } from '../../shared/currencies/base-currencies';
 
 /**
  * Data Transfer Object contains data for getting of amount of converting currency
  */
-export class GetExchangeRateDto implements IGetExchangeResult {
+export class GetExchangeResultDto implements IGetExchangeResult {
+  @IsOptional()
+  @IsString()
+  @MinLength(1)
+  @Matches(/\d{4}\-\d{2}\-\d{2}/)
+  @Expose()
+  @ApiPropertyOptional({
+    description: 'Date of exchange',
+    example: '2023-06-01',
+  })
+  date?: string;
+
   @IsString()
   @MinLength(1)
   @IsEnum(currencyCodeNames)
@@ -32,6 +45,7 @@ export class GetExchangeRateDto implements IGetExchangeResult {
   })
   targetCurrency!: currencyCode;
 
+  @Transform(({ value }) => parseFloat(value))
   @IsNumber({ allowNaN: false, allowInfinity: false })
   @IsPositive()
   @Expose()
@@ -40,4 +54,17 @@ export class GetExchangeRateDto implements IGetExchangeResult {
     example: 100,
   })
   initialAmount!: number;
+
+
+  @IsOptional()
+  @IsString()
+  @MinLength(1)
+  @IsEnum(baseCurrencies)
+  @Expose()
+  @ApiPropertyOptional({
+    description: 'Basis currency for calculation',
+    enum: baseCurrencies,
+    example: baseCurrencies['RUB'],
+  })
+  basisCurrency?: baseCurrency;
 }
