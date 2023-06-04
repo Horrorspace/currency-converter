b2ba4f3cc73a5e9da6fbb450c97e967252ef6639
Eugeniy Utkin <eugeniy.it@piar.ee>
2023-06-04 18:37:29 +0300

    configured app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { ConfigModule } from './config/config.module';
+import { CurrenciesModule } from './currencies/currencies.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { LoggerModule } from './logger/logger.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    EventEmitterModule.forRoot(),
+    ConfigModule,
+    CurrenciesModule,
+    LoggerModule,
+    PrismaModule
+  ]
 })
 export class AppModule {}
