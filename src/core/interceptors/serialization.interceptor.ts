93882c44585159ea415a85703edd0a9f1dd3e4db
Eugeniy Utkin <eugeniy.it@piar.ee>
2023-06-04 18:34:58 +0300

    created serialization decorator

---
+++ src/core/interceptors/serialization.interceptor.ts
@@ -0,0 +1,41 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { plainToInstance, ClassTransformOptions, ClassConstructor } from 'class-transformer';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+/**
+ * Serialization interceptor transforms response by filtering only exposed properties
+ */
+@Injectable()
+export class SerializationInterceptor implements NestInterceptor {
+  constructor(
+    /**
+     * Constructor of class to which the target object must satisfy
+     */
+    private readonly entity: ClassConstructor<unknown>,
+  ) {}
+
+  /**
+   * Parameters of transformation
+   */
+  private readonly options: ClassTransformOptions = {
+    excludeExtraneousValues: true,
+  };
+
+  /**
+   * Implements interception logic
+   * @param context Execution context which describes current request pipeline
+   * @param next Object which provides access to response RxJS stream
+   */
+  public intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => {
+        if (!data || typeof data !== 'object') {
+          return data;
+        }
+
+        return plainToInstance(this.entity, data, this.options);
+      }),
+    );
+  }
+}
