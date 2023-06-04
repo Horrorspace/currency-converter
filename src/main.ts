f738466b03f10624bac8a50e801723483aba0424
Eugeniy Utkin <eugeniy.it@piar.ee>
2023-06-04 18:37:40 +0300

    updated main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ApiConfigService } from './config/services/api-config.service';
+import { AppConfigService } from './config/services/app-config.service';
+import { appModes } from './config/types/app-modes';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const { host, port } = app.get(ApiConfigService);
+  const { mode } = app.get(AppConfigService);
+
+  if (mode === appModes.development) {
+    const swaggerConfig = new DocumentBuilder()
+      .setTitle('Currency exchange')
+      .setDescription('Documentation for RESTful API of currency exchange service')
+      .setVersion('1.0.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('doc', app, document);
+  }
+
+  await app.listen(port, host);
 }
 bootstrap();
